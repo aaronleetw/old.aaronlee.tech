8fba5252cda2faeff4c066d5abcb383d0485e552
Aaron <longyklee@gmail.com>
2020-08-12 18:58:08 +0800

    Updated Contact Section

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(".link").click(function () {
     var target = $(this).attr("href");
-    var locate = $(target).offset().top - $(".navbar").outerHeight();
+    var locate = $(target).offset().top - $(".navbar").outerHeight() + 14;
     var duration = 1000;
     $("html, body").stop().animate({
         scrollTop: locate
